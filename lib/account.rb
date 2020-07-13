36386bbaa36530e79a895381db72d80ba97b87e5
Nick Butler <nwmbutler@gmail.com>
2020-07-13 11:54:03 +0100

    Account accepts method deposit with one arg

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,6 @@
 class Account
+
+  def deposit(int)
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,8 @@ describe Account do
     expect(subject).to be_a_kind_of Account
   end
 
+  it "adds allows a deposit to be made" do
+    expect(subject).to respond_to(:deposit).with(1).arguments
+  end
+
 end
