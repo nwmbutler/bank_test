2d1be2322ffd59c10cc841edd06e53df27a6d4a9
Nick Butler <nwmbutler@gmail.com>
2020-07-13 14:12:42 +0100

    first statement test passing

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,25 @@
 class Account
 
-BASE_BALANCE = 0
+  BASE_BALANCE = 0
+  TIME = Time.now
 
   def initialize
     @balance = BASE_BALANCE
     @transactions = []
-    @time = Time.now
   end
 
   def deposit(value)
     @balance += value
-    @transactions.push({@time.strftime("%d/%m/%Y") => value})
+    @transactions.push({ TIME.strftime("%d/%m/%Y") => value })
   end
 
   def withdraw(value)
     @balance -= value
-    @transactions.push({@time.strftime("%d/%m/%Y") => value})
+    @transactions.push({ TIME.strftime("%d/%m/%Y") => value })
+  end
+
+  def statement
+    return @transactions
   end
 
 end
