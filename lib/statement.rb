1c750a9c51443f968e42438ef7b1f963d7092059
Nick Butler <nwmbutler@gmail.com>
2020-07-13 16:01:24 +0100

    Statement initializes with credit, debit and balance

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,2 +1,9 @@
 class Statement
+
+  def initialize(credit: nil, debit: nil, balance: nil)
+    @credit = credit
+    @debit = debit
+    @balance = balance
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,11 @@ describe Statement do
   it "is an instance of Statement" do
     expect(subject).to be_a_kind_of Statement
   end
+
+  it "initializes with credit, debit & balance = nil" do
+    expect(subject.instance_variable_get(:@balance)).to eq nil
+    expect(subject.instance_variable_get(:@credit)).to eq nil
+    expect(subject.instance_variable_get(:@debit)).to eq nil
+  end
+
 end
