d641924497757f8474d2015dd5dd5992e509ade2
Nick Butler <nwmbutler@gmail.com>
2020-07-13 12:00:42 +0100

    passing test for initialising with balance of 0

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,10 @@
 class Account
+  def initialize
+    @balance = 0
+  end
 
   def deposit(int)
+    return int
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,6 +5,10 @@ describe Account do
     expect(subject).to be_a_kind_of Account
   end
 
+  it "has an initial balance of 0 pounds" do
+    expect(subject.instance_variable_get(:@balance)).to eql(0)
+  end
+
   it "adds allows a deposit to be made" do
     expect(subject).to respond_to(:deposit).with(1).arguments
   end
