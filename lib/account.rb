e27382d82d01fdb895b5fd83e7732e0b7e18d50b
Nick Butler <nwmbutler@gmail.com>
2020-07-15 14:35:07 +0100

    unshift removed and replaced with push. Reverse reinstated

--- lib/account.rb
+++ lib/account.rb
@@ -24,19 +24,20 @@ class Account
   end
 
   def statement
-    puts @irb_topper, @transaction_record.map(&:format)
+    ascending = @transaction_record.reverse
+    puts @irb_topper, ascending.map(&:format)
   end
 
   private
 
   def deposit_update(credit: nil, balance: nil)
     update = @transaction.new(credit: credit, balance: balance)
-    @transaction_record.unshift(update)
+    @transaction_record.push(update)
   end
 
   def withdrawal_update(debit: nil, balance: nil)
     update = @transaction.new(debit: debit, balance: balance)
-    @transaction_record.unshift(update)
+    @transaction_record.push(update)
   end
 
   attr_accessor :balance
